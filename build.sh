5de8f1051d5a9f470d36c4261d4eaafd0aca4c18
lyfeyaj <lyfeyaj@gmail.com>
2015-07-24 14:16:37 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
+git checkout gh-pages
+
 pretty_doc -t default -o ./examples/default README.md
 pretty_doc -t bootstrap -o ./examples/bootstrap README.md
 pretty_doc -t parallel -o ./examples/parallel README.md
 
-pretty_doc -t parallel README.md
+pretty_doc -t parallel README.md -o .
 
 mv README.html index.html
+
+git checkout master
